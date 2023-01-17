11ab429cdd82aeaf765bc9157f9a9967005e6c0b
EdinsonSR <edinsonsr.95@gmail.com>
2023-01-17 12:48:38 -0500

    Actualizacion5

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,4 +262,26 @@
     })
   });
 
+  const btn = document.getElementById('button');
+
+  document.getElementById('form')
+   .addEventListener('submit', function(event) {
+     event.preventDefault();
+  
+     btn.value = 'Sending...';
+  
+     const serviceID = 'default_service';
+     const templateID = 'template_obgxl6o';
+  
+     emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
+
+
 })()
